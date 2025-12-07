3601340fa6fccafc051b283993dbf37a8ad59313
gabrieldwight0609 <dwilghtgabriel@gmail.com>
2025-12-07 23:57:25 +0700

    Refactor main.cpp to remove redundant code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include "Tubes.h""
+#include "Tubes.h"
 
 using namespace std;
 
 int main() {
-    // 1. Deklarasi Variabel Struktur Data
     ListLibrary Library;
     ListPlaylist Playlist;
     QueueLagu UpNext;
 
-    // 2. Inisialisasi (Create)
     createLibrary(Library);
     createPlaylist(Playlist);
     createQueue(UpNext);
 
-    // 3. Isi Data Dummy
     initData(Library);
 
-    // 4. Variabel Menu
-    int role = 1;      // Inisialisasi awal (bukan 0) agar loop jalan pertama kali
-    string inputTemp;  // Variabel bantuan untuk membaca input
+    int role = 1;     
+    string inputTemp;  
 
     // 5. Loop Menu Utama
     while (role != 0) {
@@ -30,14 +26,11 @@ int main() {
         cout << "0. Keluar" << endl;
 
         cout << "Pilihan: ";
-        getline(cin, inputTemp); // Pakai getline agar aman dari sisa buffer
+        getline(cin, inputTemp); 
 
-        // Konversi string ke integer
         if (inputTemp.empty()) {
-            role = -1; // Anggap pilihan tidak valid jika kosong
+            role = -1; 
         } else {
-            // Menggunakan stoi untuk konversi
-            // (Bisa ditambahkan try-catch jika ingin menangani input huruf)
             role = stoi(inputTemp);
         }
 
